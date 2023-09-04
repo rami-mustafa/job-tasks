616c456ae87891a3d20845729b738780407d3225
Rami Mustafa <ramimustafa77@gmail.com>
2023-09-04 15:37:13 +0200

    Design

--- task2_app/task2_app/ViewController.swift
+++ task2_app/task2_app/ViewController.swift
@@ -3,11 +3,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var messageTF: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
+    
+    @IBAction func SetAlarm(_ sender: Any) {
+    }
+    
 }
 
