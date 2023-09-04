75cdbdaf619352f4c5c1bab2a0efa79931142b8c
Rami Mustafa <ramimustafa77@gmail.com>
2023-09-04 10:32:59 +0200

    create a project

---
+++ task4_app/task4_app/task4_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  task4_appApp.swift
+//  task4_app
+//
+//  Created by Ghaiath Alhereh on 04.09.23.
+//
+
+import SwiftUI
+
+@main
+struct task4_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
