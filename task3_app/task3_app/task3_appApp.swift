0a8fe8980dc269b9518edf6e28d6768c0882091e
Rami Mustafa <ramimustafa77@gmail.com>
2023-09-05 15:01:13 +0200

    task3

---
+++ task3_app/task3_app/task3_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  task3_appApp.swift
+//  task3_app
+//
+//  Created by Ghaiath Alhereh on 05.09.23.
+//
+
+import SwiftUI
+
+@main
+struct task3_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
