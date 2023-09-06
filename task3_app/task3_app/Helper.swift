1ec1270fb823ab40dfca20d9819e08a084d9d939
Rami Mustafa <ramimustafa77@gmail.com>
2023-09-06 21:25:26 +0200

    TimerMode

---
+++ task3_app/task3_app/Helper.swift
@@ -0,0 +1,15 @@
+//
+//  Helper.swift
+//  task3_app
+//
+//  Created by Ghaiath Alhereh on 06.09.23.
+//
+
+import Foundation
+
+
+enum TimerMode {
+    case running
+    case paused
+    case initial
+}
