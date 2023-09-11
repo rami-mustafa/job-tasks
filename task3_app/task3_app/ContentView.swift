35c1d6613080589229521cb97359649a7ba3d7ca
Rami Mustafa <ramimustafa77@gmail.com>
2023-09-11 22:53:05 +0200

    View operations

--- task3_app/task3_app/ContentView.swift
+++ task3_app/task3_app/ContentView.swift
@@ -1,22 +1,77 @@
-//
-//  ContentView.swift
-//  task3_app
-//
-//  Created by Ghaiath Alhereh on 05.09.23.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var timerManager = TimerManager()
+    
+    
+    @State var timerMode: TimerMode = .initial
+    
+    @State var selectedPickerIndex = 0
+    @State var SecondselectedPickerIndex = 0
+    
+    let availableMinutes = Array(1...59)
+    let availableSecond = Array(1...59)
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            VStack {
+                Text(secondsToMinutesAndSeconds(seconds: timerManager.secondsLeft))
+                    .font(.system(size: 80))
+                    .padding(.top , 80)
+                
+                Image(systemName: timerManager.timerMode == .running ? "pause.circle.fill": "play.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180 , height: 180)
+                    .foregroundColor(.red)
+                    .onTapGesture {
+                        
+                        if self.timerManager.timerMode == .initial {
+                            self.timerManager.setTimerLength(minutes: self.availableMinutes[self.selectedPickerIndex]*60)
+                        }
+                        self.timerManager.timerMode == .running ? self.timerManager.pause() : self.timerManager.start()
+                        
+                    }
+                
+                if timerManager.timerMode == .paused {
+                    Image(systemName: "gobackward")
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                        .padding(.top , 40) .onTapGesture(perform: {
+                            self.timerManager.reset()
+                        })
+                }
+                
+                HStack{
+                    
+                    if timerManager.timerMode == .initial {
+                        Picker(selection: $selectedPickerIndex, label: Text("")) {
+                            ForEach(0 ..< availableMinutes.count) {
+                                Text("\(self.availableMinutes[$0]) min")
+                            }
+                        }
+                        
+                        Picker(selection: $SecondselectedPickerIndex, label: Text("")) {
+                            ForEach(0 ..< availableSecond.count) {
+                                Text("\(self.availableSecond[$0]) s")
+                            }
+                        }
+                        
+                    }
+                    
+                }
+                Spacer()
+            }
+            .navigationBarTitle("RAMİ")
         }
-        .padding()
+        .environment(\.colorScheme, .dark)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +79,10 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+
+
